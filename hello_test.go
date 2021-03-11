e69e72e544e8f26653d8b5285a37b34d8bf9ce8e
Omkar Deshmukh <omdesh.0407@gmail.com>
2021-03-11 19:30:18 +0530

    Adding first prog and test

---
+++ hello_test.go
@@ -0,0 +1,13 @@
+package main
+
+import "testing"
+
+//The test function must start with the word Test
+func TestHello(t *testing.T) {
+	got := hello()
+	want := "Hello World"
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
