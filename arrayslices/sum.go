a6009a79e132db45f9d8ee896bba6c06b77bc571
Omkar Deshmukh <omdesh.0407@gmail.com>
2021-03-13 17:14:08 +0530

    Adding Tests and Benchmarks for Arrays and Slices

--- arrayslices/sum.go
+++ arrayslices/sum.go
@@ -1,7 +1,16 @@
 package arrayslices
 
 //Sum numbers passed
-func Sum(numbers [5]int) int {
+func SumArray(numbers [5]int) int {
+
+	sum := 0
+	for _, number := range numbers {
+		sum += number
+	}
+	return sum
+}
+
+func Sum(numbers []int) int {
 
 	sum := 0
 	for _, number := range numbers {
--- arrayslices/sum_test.go
+++ arrayslices/sum_test.go
@@ -3,13 +3,41 @@ package arrayslices
 import "testing"
 
 func TestSum(t *testing.T) {
+	t.Run("Adding array of a set size", func(t *testing.T) {
+		numbers := [5]int{1, 2, 3, 4, 5}
 
-    numbers := [5]int{1, 2, 3, 4, 5}
+		got := SumArray(numbers)
+		want := 15
+		if got != want {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
 
-    got := Sum(numbers)
-    want := 15
+	t.Run("Testing sum using slice", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+		got := Sum(numbers)
+		want := 6
+		if got != want {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
 
-    if got != want {
-        t.Errorf("got %d want %d given, %v", got, want, numbers)
-    }
+	})
+
+	t.Run("Testing sum using slice", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
+		got := Sum(numbers)
+		want := 15
+		if got != want {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+
+	})
+}
+
+func BenchmarkSum(b *testing.B) {
+	Sum([]int{1, 2, 3, 4, 5})
+}
+
+func BenchmarkArray(b *testing.B) {
+	SumArray([5]int{1, 2, 3, 4, 5})
 }
