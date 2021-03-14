218d1de91f65326f7530f76afbe4c38abc9e363b
Omkar Deshmukh <omdesh.0407@gmail.com>
2021-03-14 18:56:54 +0530

    Adding pointer module with wallet example

---
+++ pointerserrors/wallet_test.go
@@ -0,0 +1,41 @@
+package pointerserrors
+
+import "testing"
+
+func TestWallet(t *testing.T) {
+
+	assertBalance := func(t *testing.T, w Wallet, want Bitcoin) {
+		t.Helper()
+		got := w.Balance()
+
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	}
+
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
+
+		wallet.Deposit(Bitcoin(10))
+
+		assertBalance(t, wallet, Bitcoin(10))
+		
+	})
+
+	t.Run("Withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+
+		wallet.Withdraw(Bitcoin(10))
+
+		assertBalance(t, wallet, Bitcoin(10))
+
+	})
+}
+
+func TestStringer(t *testing.T) {
+	got := Bitcoin(10).String()
+	want := "10 BTC"
+	if got != want {
+		t.Errorf("got %s want %s", got, want)
+	}
+}
