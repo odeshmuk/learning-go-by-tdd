79672df5e8e07632db04f67dcfae7835594c65e9
Omkar Deshmukh <omdesh.0407@gmail.com>
2021-03-17 02:04:34 +0530

    adding error for notfound cases in maps

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,5 +1,20 @@
 package maps
 
-func Search(dictionary map[string]string, key string) string {
-	return dictionary[key]
+import "errors"
+
+//A type created for the map of string,string
+type Dictionary map[string]string
+
+//defining an error type for errors not found
+var ErrNotFound = errors.New("could not find the word you were looking for")
+
+//returns the result,error as part of this func
+func (dictionary Dictionary) Search(key string) (string, error) {
+	definition, ok := dictionary[key]
+	//if not ok, return the group below
+	if !ok {
+		return "", ErrNotFound
+	}
+	//return this if a-ok
+	return definition, nil
 }
--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -5,13 +5,30 @@ import (
 )
 
 func TestSearch(t *testing.T) {
-	dictionary := map[string]string{"test": "testing a map"}
-	got := Search(dictionary, "test")
-	want := "testing a map"
-	assertSearch(t, got, want)
+	dictionary := Dictionary{"test": "testing a map"}
+	t.Run("known word", func(t *testing.T) {
+		got, err := dictionary.Search("test")
+		want := "testing a map"
+		assertStrings(t, got, want)
+		if err != nil {
+			t.Fatal("Expected no error")
+		}
+	})
+
+	t.Run("unknown word", func(t *testing.T) {
+		_, err := dictionary.Search("unknown")
+		want := "could not find the word you were searching"
+
+		if err == nil {
+			t.Fatal("expected an error")
+		}
+
+		assertStrings(t, err.Error(), want)
+	})
+
 }
 
-func assertSearch(t testing.TB, got, want string) {
+func assertStrings(t testing.TB, got, want string) {
 	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q given", got, want)
