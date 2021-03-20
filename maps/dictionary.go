d257dc46242c0e99900c858a88000c702c114249
Omkar Deshmukh <omdesh.0407@gmail.com>
2021-03-20 17:19:19 +0530

    Adding CRUD for maps

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -8,6 +8,12 @@ type Dictionary map[string]string
 //defining an error type for errors not found
 var ErrNotFound = errors.New("could not find the word you were searching")
 
+//defining an error type for an already existing key in dictionary
+var ErrAlreadyExists = errors.New("already existing")
+
+//defining that the key doesn't exist
+var ErrKeyDoesntExist = errors.New("key doesn't exist")
+
 //returns the result,error as part of this func
 func (dictionary Dictionary) Search(key string) (string, error) {
 	definition, ok := dictionary[key]
@@ -17,4 +23,38 @@ func (dictionary Dictionary) Search(key string) (string, error) {
 	}
 	//return this if a-ok
 	return definition, nil
+}
+
+func (dictionary Dictionary) Add(key, value string) error {
+	_, err := dictionary.Search(key)
+	switch err {
+	case ErrNotFound:
+		dictionary[key] = value
+	case nil:
+		return ErrAlreadyExists
+	}
+	return nil
+
+}
+
+func (dictionary Dictionary) Update(key, updatedValue string) error {
+	_, err := dictionary.Search(key)
+	switch err {
+	case ErrNotFound:
+		return ErrKeyDoesntExist
+	case nil:
+		dictionary[key] = updatedValue
+	}
+	return nil
+}
+
+func (dictionary Dictionary) Delete(key string) error {
+	_, err := dictionary.Search(key)
+	switch err {
+	case ErrNotFound:
+		return ErrKeyDoesntExist
+	case nil:
+		delete(dictionary, key)
+	}
+	return nil
 }
--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -19,13 +19,67 @@ func TestSearch(t *testing.T) {
 		_, err := dictionary.Search("unknown")
 		want := "could not find the word you were searching"
 
-		if err == nil {
-			t.Fatal("expected an error")
-		}
-
 		assertStrings(t, err.Error(), want)
 	})
 
+}
+
+func TestAdd(t *testing.T) {
+	dictionary := Dictionary{}
+	t.Run("Adding a word to the dictionary", func(t *testing.T) {
+		dictionary.Add("newKey", "This is a new value")
+		got, _ := dictionary.Search("newKey")
+		want := "This is a new value"
+
+		assertStrings(t, got, want)
+	})
+	t.Run("Adding an already existing word", func(t *testing.T) {
+		key := "alreadyExistingKey"
+		value := "Already Existing"
+		dictionary := Dictionary{key: value}
+		got := dictionary.Add(key, value)
+		want := "already existing"
+
+		assertStrings(t, got.Error(), want)
+	})
+}
+
+func TestUpdate(t *testing.T) {
+	key := "alreadyExistingKey"
+	value := "sample text"
+	dictionary := Dictionary{key: value}
+	t.Run("Updating an existing key", func(t *testing.T) {
+		dictionary.Update(key, "Updated Sample Text")
+		got, _ := dictionary.Search(key)
+		want := "Updated Sample Text"
+		assertStrings(t, got, want)
+	})
+
+	t.Run("Updating a key that doesn't exist", func(t *testing.T) {
+		err := dictionary.Update("notReallyAKey", "Doesn't really matter, not updating anyway")
+		want := "key doesn't exist"
+		assertStrings(t, err.Error(), want)
+	})
+}
+
+func TestDelete(t *testing.T) {
+	key := "alreadyExistingKey"
+	value := "sample text"
+	dictionary := Dictionary{key: value}
+	t.Run("Deleting a key", func(t *testing.T) {
+		dictionary.Delete(key)
+		_, gotError := dictionary.Search(key)
+		want := "could not find the word you were searching"
+
+		assertStrings(t, gotError.Error(), want)
+	})
+
+	t.Run("Deleting a key that doesn't exist", func(t *testing.T) {
+		err := dictionary.Delete("notExistentKey")
+		want := "key doesn't exist"
+
+		assertStrings(t, err.Error(), want)
+	})
 }
 
 func assertStrings(t testing.TB, got, want string) {
